e47b228d6994bde9c3e0684b441f249530847b0b
tomek <tomeksochacki@wp.pl>
2020-06-03 11:28:42 +0200

    Update

---
+++ src/com/test/example/constructorexample/PointExample.java
@@ -0,0 +1,30 @@
+package com.test.example.constructorexample;
+
+public class PointExample {
+
+    public static void main(String[] args) {
+
+        Point p = new Point(3,5);
+        Point p2 = new Point(4,6);
+        Point p3 = new Point(1,7);
+        //p.x = 3;
+        //p.y = 4;
+        System.out.println("p.x =" + p.x + ", p.y =" + p.y);
+        System.out.println("p2.x =" + p2.x + ", p2.y =" + p2.y);
+        System.out.println("p3.x =" + p3.x + ", p3.y =" + p3.y);
+    }
+}
+
+class Point{
+    Point(){
+        System.out.println("Induce constructor default");
+    }
+    Point(int first, int second){
+        System.out.println("Induce constructor with two parameters");
+        x = first;
+        y = second;
+    }
+
+    int x;
+    int y;
+}
