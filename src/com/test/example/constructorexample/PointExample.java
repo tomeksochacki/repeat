a176cfb1c6f9ab361ff86ff0f4640a730107deb0
tomek <tomeksochacki@wp.pl>
2020-06-03 12:14:36 +0200

    Update

--- src/com/test/example/constructorexample/PointExample.java
+++ src/com/test/example/constructorexample/PointExample.java
@@ -12,6 +12,19 @@ public class PointExample {
         System.out.println("p.x =" + p.x + ", p.y =" + p.y);
         System.out.println("p2.x =" + p2.x + ", p2.y =" + p2.y);
         System.out.println("p3.x =" + p3.x + ", p3.y =" + p3.y);
+
+        Car[] cars = new Car[4];
+        cars[0] = new Car("Opel");
+        cars[1] = new Car("Audi");
+        cars[2] = new Car("BMW");
+        cars[3] = new Car("Nissan");
+
+        System.out.println();
+        System.out.println("Car: ");
+        System.out.println(cars[0].getName());
+        System.out.println(cars[1].getName());
+        System.out.println(cars[2].getName());
+        System.out.println(cars[3].getName());
     }
 }
 
@@ -27,4 +40,16 @@ class Point{
 
     int x;
     int y;
+}
+
+class Car{
+    Car(String name){
+        this.name = name;
+    }
+
+    String name;
+
+    String getName(){
+        return name;
+    }
 }
